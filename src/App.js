ead5cf7c9ebd3884f54dc25098bc6ff8c2f1aec7
Yasshhh12 <prasyast152@gmail.com>
2024-05-16 19:42:10 +0530

    used callback func to generate pass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
+
+import { useCallback, useState } from "react";
 
 function App() {
+
+  const [length,setlength] = useState(8);
+  const [numberAllowed,setNumberAllowed] = useState(false);
+  const [charAllowed,setCharAllowed] = useState(false);
+  const [password,setPassword] = useState("");
+
+  const passwordGenerator = useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowed)
+    {
+      str += "0123456789"
+    }
+    
+    if(charAllowed)
+    {
+      str += "!@#$%^&*()~_+{}|"
+    }
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random()*str.length+1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+    
+  },[length,numberAllowed,charAllowed,setPassword])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+    <h1 className="text-white text-center">Password Generator</h1>
+    <div className="flex-shadow rounded-lg overflow-hidden mb-4">
+     <input 
+     type="text" 
+     value={password}
+     className="outline-none w-full py-1 px-3"
+     placeholder="Password"
+     readOnly
+     />
+    </div>
     </div>
   );
 }
